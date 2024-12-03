a6779cf29f3d8341ab95a9a99340fd58eeb7031f
Beam <b3bb0z@gmail.com>
2024-12-03 21:35:18 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int number, evenCount = 0, oddCount = 0;
+
+    while (true) {
+        cout << "Enter an integer: ";
+        cin >> number;
+
+        if (number == 0) {
+            break;
+        }
+
+        if (number % 2 == 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+
+    cout << "#Even numbers = " << evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
+
     return 0;
 }
